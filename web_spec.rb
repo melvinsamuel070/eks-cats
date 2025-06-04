e3576211a1b264f324a96752d6f9fff025d20daf
melvinsamuel070 <melvinsamuel070@gmail.com>
2025-06-04 10:22:17 +0100

    new

---
+++ web_spec.rb
@@ -0,0 +1,26 @@
+require 'rack/test'
+require_relative '/home/melvin/cats' # update with your actual path
+
+RSpec.describe Cats::Web do
+  include Rack::Test::Methods
+
+
+const promBundle = require("express-prom-bundle");
+const metricsMiddleware = promBundle({ includeMethod: true });
+app.use(metricsMiddleware);
+// metrics available at /metrics
+
+  def app
+    Cats::Web.new
+  end
+
+
+
+  describe 'GET /health' do
+    it 'returns status 200 and OK' do
+      get '/health'
+      expect(last_response.status).to eq(200)
+      expect(last_response.body).to eq('OK')
+    end
+  end
+end
