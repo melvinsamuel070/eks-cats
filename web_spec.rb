6a82a6317d01206281ac6b8330a5fc3e81d431f3
melvinsamuel070 <melvinsamuel070@gmail.com>
2025-06-05 10:04:29 +0100

    new

--- web_spec.rb
+++ web_spec.rb
@@ -1,26 +1,13 @@
 require 'rack/test'
-require_relative '/home/melvin/cats' # update with your actual path
+require 'rspec'
+require_relative '/home/melvin/cats' # Make sure this points to your actual Sinatra app file
 
 RSpec.describe Cats::Web do
   include Rack::Test::Methods
 
-
-const promBundle = require("express-prom-bundle");
-const metricsMiddleware = promBundle({ includeMethod: true });
-app.use(metricsMiddleware);
-// metrics available at /metrics
-
   def app
     Cats::Web.new
   end
-
-  RSpec.describe 'Sinatra App' do
-  include Rack::Test::Methods
-
-  def app
-    Sinatra::Application
-  end
-
 
   describe 'GET /health' do
     it 'returns status 200 and OK' do
@@ -30,6 +17,5 @@ app.use(metricsMiddleware);
     end
   end
 end
-
 
 
